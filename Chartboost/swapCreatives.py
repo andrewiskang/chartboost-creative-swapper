f4bd17174ebd97c086a1340e5aed24e5555c9df2
Andrew Kang <andrewiskang@gmail.com>
2017-02-01 14:11:30 -0800

    Added simple error tracking, outputted to CSV file. Removed sample CSV test file

--- Chartboost/swapCreatives.py
+++ Chartboost/swapCreatives.py
@@ -17,11 +17,17 @@ user32 = ctypes.windll.user32
 
 # We'll use the given CSV to compile a dictionary
 # of campaign IDs and a list of creatives to turn
-f = open(sys.argv[3])
-csv_f = csv.reader(f)
 creativeList = {}
-for row in csv_f:
-	creativeList.setdefault(row[0],[]).append(row[1])
+with open(sys.argv[3]) as f:
+	csv_f = csv.reader(f)
+	for row in csv_f:
+		creativeList.setdefault(row[0],[]).append(row[1])
+
+
+# Also open a file to record any errors
+outputFile = open('errors.csv', 'w', newline='')
+outputWriter = csv.writer(outputFile)
+outputWriter.writerow([time.localtime()])
 
 # Opens a Chrome window not signed in to any Google profile
 # loads the Chartboost website to log in
@@ -44,49 +50,65 @@ passwordInput.send_keys(password)
 
 browser.find_element_by_class_name('login-layout__submit').click()
 
-
 # For every campaign ID in the CSV:
 for campaignID in creativeList.keys():
-	# Navigate to our advertising campaigns
-	time.sleep(3)
-	browser.get('https://dashboard.chartboost.com/all/campaigns/' + campaignID)
-	print('Navigating to '+campaignID)
-	time.sleep(10)
-	print('**REMOVING CREATIVES**')
-	time.sleep(5)
-	removeButtons = browser.find_elements_by_css_selector('i.ui.minus.creatives.icon')
-	print(str(len(removeButtons)) + ' creatives to remove:')
-	for button in removeButtons:
-		button.click()
-		print('creative removed')
+	# Error checking: tries to turn creatives,
+	# returns campaign IDs that were not turned
+	try:
+		# Navigate to our advertising campaigns
+		time.sleep(3)
+		browser.get('https://dashboard.chartboost.com/all/campaigns/' + campaignID)
+		print('Navigating to '+campaignID)
+		time.sleep(10)
+		print('**REMOVING CREATIVES**')
+		time.sleep(5)
+		removeButtons = browser.find_elements_by_css_selector('i.ui.minus.creatives.icon')
+		if len(removeButtons) == 0:
+			raise ValueError('0 creatives to remove')
+		print(str(len(removeButtons)) + ' creatives to remove:')
+		time.sleep(2)
+		for button in removeButtons:
+			button.click()
+			print('creative removed')
+			time.sleep(2)
+
+		# Add in creatives:
+		# - Press "Select Existing"
+		# - Search for a given creative name
+		# - Tick the box to the left
+		# - Press "Confirm"
+
+		print('**TURNING IN CREATIVES**')
 		time.sleep(2)
 
-	# Add in creatives:
-	# - Press "Select Existing"
-	# - Search for a given creative name
-	# - Tick the box to the left
-	# - Press "Confirm"
-
-	print('**TURNING IN CREATIVES**')
-	time.sleep(2)
-
-	browser.find_element_by_id('network-advertiser__target-0__creatives__actions--select-existing').click()
-	time.sleep(2)
-
-	# For now, assume a list of creatives to turn in
-
-	for creative in creativeList[campaignID]:
-		elem = browser.find_element_by_xpath("//div[contains(text(), '" + creative + "')]/preceding-sibling::div[2]")
-		elem.click()
-		print('+ ' + creative)
+		browser.find_element_by_id('network-advertiser__target-0__creatives__actions--select-existing').click()
 		time.sleep(2)
 
+		# For now, assume a list of creatives to turn in
 
-	browser.find_element_by_id('network-advertiser__target-0__creatives__actions--confirm').click()
-	time.sleep(3)
+		for creative in creativeList[campaignID]:
+			elem = browser.find_element_by_xpath("//div[contains(text(), '" + creative + "')]/preceding-sibling::div[2]")
+			elem.click()
+			print('+ ' + creative)
+			time.sleep(2)
 
-	# Save your progress - press the save button
-	# browser.find_element_by_id('network-advertiser__enabled-actions--save').click()
-	time.sleep(10)
-	print('Creative turn for '+campaignID+' completed\n')
+
+		browser.find_element_by_id('network-advertiser__target-0__creatives__actions--confirm').click()
+		time.sleep(3)
+
+		# Save your progress - press the save button
+		browser.find_element_by_id('network-advertiser__enabled-actions--save').click()
+		time.sleep(10)
+		print('Creative turn for ' + campaignID + ' completed\n')
+	except Exception as e:
+		print('Error in campaign ' + campaignID + ': ' + str(e) + '\n')
+		outputWriter.writerow([campaignID, e])
+		continue
+	#except Exception as e:
+	#	print('Error for campaign', campaignID, ':', e)
+	#	outputWriter.writerow(list(campaignID, e))
+	#	continue
+
+
+outputFile.close()
 browser.quit()
